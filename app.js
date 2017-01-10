4ad69101f68ca70215bc01ab113e37c593b7c565
j-west <westja89@gmail.com>
2017-01-09 22:33:06 -0600

    Add listeners to find mew movies and search your movies links.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+$("#new-movies").click(function () {
+  $(".movieDB-view").addClass("hide");
+  $(".search-result").removeClass("hide");
+});
 
+$("#movie-db").click(function () {
+  $(".movieDB-view").removeClass("hide");
+  $(".search-result").addClass("hide");
+});
 
 
 $("#searchBtn").click(function() {
-  console.log("it works");
+  //console.log("it works");
   $.ajax({
     url: `http://www.omdbapi.com/?t=${$("#userInput").val()}&y=&plot=full&r=json`,
     dataType: 'json',
     success: function(data) {
       console.log(data);
+      $("#userInput").val("");
       $(".search-result").html(`
                                 <img src="${data.Poster}" alt="movie cover image">
                                 <h2>${data.Title}</h2>
