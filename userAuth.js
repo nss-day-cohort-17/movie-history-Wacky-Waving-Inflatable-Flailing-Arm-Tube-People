34265474a7baede795fbfaa2ea5a5edd3081327a
j-west <westja89@gmail.com>
2017-01-13 13:33:21 -0600

    Add userAuth file with starter firebase config info

---
+++ userAuth.js
@@ -0,0 +1,17 @@
+var target;
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDy-m7zSm8n2VvqQpDcH4g2YpbgkaYNoPc",
+  authDomain: "test-351e3.firebaseapp.com",
+  databaseURL: "https://test-351e3.firebaseio.com",
+  storageBucket: "test-351e3.appspot.com",
+  messagingSenderId: "117618747620"
+});
+
+
+
+
+
+
+
