bce796b57d0cabd01fefe24c919f327d21cb3118
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-15 16:40:51 -0600

    added font-awsome to dependencies; inserted stars into card; added logic to star click

--- app.js
+++ app.js
@@ -117,6 +117,28 @@ function yourMovies(data) {
                                     <img class="img-responsive card-img-top center-block" src="${data[id].Poster}" alt="${data[id].Title}">
                                     <div class="card-block">
                                     <h3 class="card-title">${data[id].Title}</h4>
+                                        <section class='rating-widget'>
+                                          <div class='rating-stars text-center'>
+                                            <ul id='stars'>
+                                              <li class='star' title='Poor' data-value='1'>
+                                                <i class='fa fa-star fa-fw'></i>
+                                              </li>
+                                              <li class='star' title='Fair' data-value='2'>
+                                                <i class='fa fa-star fa-fw'></i>
+                                              </li>
+                                              <li class='star' title='Good' data-value='3'>
+                                                <i class='fa fa-star fa-fw'></i>
+                                              </li>
+                                              <li class='star' title='Excellent' data-value='4'>
+                                                <i class='fa fa-star fa-fw'></i>
+                                              </li>
+                                              <li class='star' title='WOW!!!' data-value='5'>
+                                                <i class='fa fa-star fa-fw'></i>
+                                              </li>
+                                            </ul>
+                                          </div>
+                                        </section>
+
                 <button type="button" class="btn-list btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg${i}">More Info</button>
 
                 <div class="modal fade bs-example-modal-lg${i}" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
@@ -140,6 +162,7 @@ function yourMovies(data) {
 
     addListenersToListViews()
     removeCard(data)
+    rate()
 }
 
 
@@ -178,3 +201,65 @@ function removeCard(data){
 $('#myModal').modal('hide');
 $('body').removeClass('modal-open');
 $('.modal-backdrop').remove();
+
+
+
+
+function rate(data){
+
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $('#stars li').on('mouseover', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+    // Now highlight all the stars that's not after the current hovered star
+    $(this).parent().children('li.star').each(function(e){
+      if (e < onStar) {
+        $(this).addClass('hover');
+      }
+      else {
+        $(this).removeClass('hover');
+      }
+    });
+
+  }).on('mouseout', function(){
+    $(this).parent().children('li.star').each(function(e){
+      $(this).removeClass('hover');
+    });
+  });
+
+
+  /* 2. Action to perform on click */
+  $('#stars li').on('click', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+    var stars = $(this).parent().children('li.star');
+
+    for (i = 0; i < stars.length; i++) {
+      $(stars[i]).removeClass('selected');
+    }
+
+    for (i = 0; i < onStar; i++) {
+      $(stars[i]).addClass('selected');
+    }
+
+    // JUST RESPONSE (Not needed)
+    var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+    var msg = "";
+    if (ratingValue > 1) {
+        msg = "Thanks! You rated this " + ratingValue + " stars.";
+    }
+    else {
+        msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+    }
+    responseMessage(msg);
+
+  });
+
+
+};
+
+
+function responseMessage(msg) {
+    alert(msg)
+  // $('.success-box').fadeIn(200);
+  // $('.success-box div.text-message').html("<span>" + msg + "</span>");
+}
